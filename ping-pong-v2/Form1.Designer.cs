6788980939c417fbb11695fc95b587363432aded
Yordan Yordanov <41238297+Tsvetomirov@users.noreply.github.com>
2019-04-06 11:26:58 +0300

    Add files via upload

---
+++ ping-pong-v2/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Ping_Pong_v2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Ball = new System.Windows.Forms.PictureBox();
+            this.Slider = new System.Windows.Forms.PictureBox();
+            this.Vert = new System.Windows.Forms.Timer(this.components);
+            this.Horz = new System.Windows.Forms.Timer(this.components);
+            this.key_watch = new System.Windows.Forms.Timer(this.components);
+            this.pblpause = new System.Windows.Forms.Panel();
+            this.bunifuCustomLabel1 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.bunifuImageButton1 = new Bunifu.Framework.UI.BunifuImageButton();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Slider)).BeginInit();
+            this.pblpause.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Ball
+            // 
+            this.Ball.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Ball.Enabled = false;
+            this.Ball.Image = ((System.Drawing.Image)(resources.GetObject("Ball.Image")));
+            this.Ball.Location = new System.Drawing.Point(0, 0);
+            this.Ball.Name = "Ball";
+            this.Ball.Size = new System.Drawing.Size(65, 65);
+            this.Ball.TabIndex = 0;
+            this.Ball.TabStop = false;
+            // 
+            // Slider
+            // 
+            this.Slider.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.Slider.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.Slider.Location = new System.Drawing.Point(279, 432);
+            this.Slider.Name = "Slider";
+            this.Slider.Size = new System.Drawing.Size(183, 17);
+            this.Slider.TabIndex = 1;
+            this.Slider.TabStop = false;
+            // 
+            // Vert
+            // 
+            this.Vert.Enabled = true;
+            this.Vert.Interval = 1;
+            this.Vert.Tick += new System.EventHandler(this.Vert_Tick);
+            // 
+            // Horz
+            // 
+            this.Horz.Enabled = true;
+            this.Horz.Tick += new System.EventHandler(this.Horz_Tick);
+            // 
+            // key_watch
+            // 
+            this.key_watch.Enabled = true;
+            this.key_watch.Interval = 1;
+            this.key_watch.Tick += new System.EventHandler(this.key_watch_Tick);
+            // 
+            // pblpause
+            // 
+            this.pblpause.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pblpause.Controls.Add(this.bunifuCustomLabel1);
+            this.pblpause.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pblpause.Location = new System.Drawing.Point(0, 0);
+            this.pblpause.Name = "pblpause";
+            this.pblpause.Size = new System.Drawing.Size(800, 56);
+            this.pblpause.TabIndex = 3;
+            // 
+            // bunifuCustomLabel1
+            // 
+            this.bunifuCustomLabel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.bunifuCustomLabel1.AutoSize = true;
+            this.bunifuCustomLabel1.Font = new System.Drawing.Font("Segoe Print", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuCustomLabel1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.bunifuCustomLabel1.Location = new System.Drawing.Point(353, 10);
+            this.bunifuCustomLabel1.Name = "bunifuCustomLabel1";
+            this.bunifuCustomLabel1.Size = new System.Drawing.Size(158, 36);
+            this.bunifuCustomLabel1.TabIndex = 0;
+            this.bunifuCustomLabel1.Text = "Game Paused";
+            this.bunifuCustomLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // bunifuImageButton1
+            // 
+            this.bunifuImageButton1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.bunifuImageButton1.BackColor = System.Drawing.Color.Red;
+            this.bunifuImageButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bunifuImageButton1.Image = ((System.Drawing.Image)(resources.GetObject("bunifuImageButton1.Image")));
+            this.bunifuImageButton1.ImageActive = null;
+            this.bunifuImageButton1.Location = new System.Drawing.Point(772, 0);
+            this.bunifuImageButton1.Name = "bunifuImageButton1";
+            this.bunifuImageButton1.Size = new System.Drawing.Size(29, 27);
+            this.bunifuImageButton1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bunifuImageButton1.TabIndex = 2;
+            this.bunifuImageButton1.TabStop = false;
+            this.bunifuImageButton1.Zoom = 10;
+            this.bunifuImageButton1.Click += new System.EventHandler(this.bunifuImageButton1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pblpause);
+            this.Controls.Add(this.bunifuImageButton1);
+            this.Controls.Add(this.Ball);
+            this.Controls.Add(this.Slider);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Slider)).EndInit();
+            this.pblpause.ResumeLayout(false);
+            this.pblpause.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Ball;
+        private System.Windows.Forms.PictureBox Slider;
+        private System.Windows.Forms.Timer Vert;
+        private System.Windows.Forms.Timer Horz;
+        private System.Windows.Forms.Timer key_watch;
+        private System.Windows.Forms.Panel pblpause;
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel1;
+        private Bunifu.Framework.UI.BunifuImageButton bunifuImageButton1;
+    }
+}
+
---
+++ ping-pong-v2/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ping_Pong_v2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int speed = 5;
+        int vdir = +1;
+        int slider_speed = 10;
+        bool gameOver = false;
+
+        
+        private void Vert_Tick(object sender, EventArgs e)
+        {
+            
+            if (Ball.Top < 0)
+            {
+
+                vdir = +1;
+            }
+            else if (Ball.Top > (this.Height - (Ball.Height + Slider.Height)))
+            {
+
+
+
+                if (Ball.Left < Slider.Left ||(Ball.Left+Ball.Width) > (Slider.Left+Slider.Width))
+                {
+                    Vert.Enabled = Horz.Enabled = false;
+                    bunifuCustomLabel1.BackColor = Color.DarkRed;
+                    bunifuCustomLabel1.Text = "Замина";
+                   this.Enabled = false;
+                }
+                else
+                {
+                    speed++;
+                }
+
+
+
+
+
+                vdir = -1;
+            }
+                Ball.Top += (vdir*speed);
+
+
+        }
+
+
+        int hdir = +1;
+        private void Horz_Tick(object sender, EventArgs e)
+        {
+
+            if (Ball.Left < 0)
+            {
+                hdir = +1;
+            }
+            else if (Ball.Left > (this.Width - Ball.Width))
+            {
+                hdir = -1;
+            }
+
+                Ball.Left += (hdir*speed);
+        }
+
+        private void bunifuImageButton1_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+
+
+
+        bool _left = false, _right = false;
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //active
+            if (e.KeyCode ==Keys.Right)
+            {
+                _right = true;
+            }else if (e.KeyCode == Keys.Left)
+            {
+                _left = true;
+            }else if (e.KeyCode == Keys.Escape)
+            {
+                //pause
+                Vert.Enabled = Horz.Enabled = paused = false;
+
+            }
+            else if (e.KeyCode == Keys.End)
+            {
+                //resume
+                Vert.Enabled = Horz.Enabled = paused = true;
+
+            }
+
+
+        }
+        bool paused
+        {
+            get
+            {
+                return !pblpause.Visible;
+            }
+            set
+            {
+                pblpause.Visible = !value;
+            }
+        }
+
+        private void key_watch_Tick(object sender, EventArgs e)
+        {
+            if (_right && Slider.Left <(this.Width- Slider.Width))
+            {
+                Slider.Left = Slider.Left + 10;
+
+            }else if (_left && Slider.Left > 0)
+            {  
+                Slider.Left = Slider.Left - 10;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            //inactive
+            if (e.KeyCode == Keys.Left || e.KeyCode == Keys.Right)
+            {
+                _right = _left = false; // disable 
+            }
+        }
+    }
+}
